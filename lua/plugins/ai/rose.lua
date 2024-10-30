009f14db701555f2c7eb6a877b5bac5f42e0ed2b
Lucky Pony <pony@arch>
2024-10-30 14:23:17 -0700

    updated rose with noise and telescope UI

--- lua/plugins/ai/rose.lua
+++ lua/plugins/ai/rose.lua
@@ -5,15 +5,34 @@ return {
   keys = {
     { "<leader>qr", ":Rose<CR>", desc = "[q]ompass [r]ose" },
   },
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+    "folke/noice.nvim",
+  },
   opts = {
     model = "llama3.2:3b",
     display_mode = "float",
     show_prompt = true,
     show_model = true,
     no_auto_close = false,
+    telescope_opts = {
+      theme = "dropdown",
+    },
   },
   config = function(_, opts)
     require("rose").setup(opts)
+
+    require("noice").setup {
+      lsp = {
+        hover = {
+          enabled = true,
+        },
+      },
+      presets = {
+        bottom_search = true,
+        command_palette = true,
+      },
+    }
     pcall(io.popen, "ollama serve > /dev/null 2>&1 &")
   end,
 }
