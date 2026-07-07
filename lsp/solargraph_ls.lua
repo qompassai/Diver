b0b632054521e675ed034d2665d72849206fad34
Matt Porter <map@qompass.ai>
2026-07-06 20:35:12 -0700

    update

--- lsp/solargraph_ls.lua
+++ lsp/solargraph_ls.lua
@@ -3,25 +3,71 @@
 -- Copyright (C) 2025 Qompass AI, All rights reserved
 -- ---------------------------------------------------
 -- References:  https://solargaraph.org/
----@type vim.lsp.Config
-return {
-    cmd = {
-        'solargraph', ---@type string[]
-        'stdio',
+return ---@type vim.lsp.Config
+{
+  cmd = {
+    'solargraph',
+    'stdio',
+  },
+  filetypes = {
+    'ruby',
+  },
+  root_markers = {
+    'Gemfile',
+    '.solargraph.yml',
+    'Fastfile',
+    'Appfile',
+    'Pluginfile',
+    '.ruby-version',
+    '.git',
+  },
+  init_options = {
+    formatting = false,
+  },
+  settings = {
+    solargraph = {
+      include = {
+        '**/*.rb',
+      },
+      exclude = {
+        'spec/**/*',
+        'test/**/*',
+        'vendor/**/*',
+        '.bundle/**/*',
+      },
+      require = {
+        --'rails',
+      },
+      domains = {},
+      reporters = {
+        'rubocop',
+        'require_not_found',
+        'typecheck:strict',
+      },
+      formatter = 'rubocop',
+      rubocop = {
+        cops = 'safe',
+        except = {},
+        only = {},
+        extra_args = {},
+      },
+      requirePaths = {},
+      plugins = {
+        'solargraph-rails',
+      },
+      maxFiles = 5000,
+      diagnostics = false,
+      formatting = false,
+      autoformat = false,
+      useBundler = false,
+      completion = true,
+      hover = true,
+      symbols = true,
+      definitions = true,
+      references = true,
+      rename = true,
+      folding = true,
+      checkGemVersion = false,
     },
-    settings = {
-        solargraph = {
-            diagnostics = true, ---@type boolean
-        },
-    },
-    init_options = {
-        formatting = true,
-    },
-    filetypes = { ---@type string[]
-        'ruby',
-    },
-    root_markers = { ---@type string[]
-        'Gemfile',
-        '.git',
-    },
+  },
 }
