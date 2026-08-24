3fa94e33c4b203c63368e3fe80fdefcb5eca2d2d
Matt Porter <map@qompass.ai>
2026-08-24 08:48:38 -0700

    Update checkpatch.lua

--- lua/linters/checkpatch.lua
+++ lua/linters/checkpatch.lua
@@ -15,6 +15,7 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 -- #################################################################
+---@source https://docs.kernel.org/dev-tools/checkpatch.html
 
 local diagnostic = vim.diagnostic
 local fs = vim.fs
