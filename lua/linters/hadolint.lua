873e7fd70034c553fc00be775adf6a7ec8a7c192
Matt Porter <map@qompass.ai>
2026-08-24 21:18:29 -0700

    Update hadolint.lua

--- lua/linters/hadolint.lua
+++ lua/linters/hadolint.lua
@@ -1,5 +1,5 @@
 -- #################################################################
--- /qompassai/lua/linters/hadolint.lua
+-- /qompassai/diver/lua/linters/hadolint.lua
 -- Qompass AI Hadolint
 -- SPDX-License-Identifier: Apache-2.0
 -- Copyright (c) 2026 Qompass AI
