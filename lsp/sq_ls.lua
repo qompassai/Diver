bce087cd2660b50178bdc871fe707d0a484379fd
Matt Porter <map@qompass.ai>
2026-08-31 19:59:14 -0700

    Update sq_ls.lua

--- lsp/sq_ls.lua
+++ lsp/sq_ls.lua
@@ -1,47 +1,69 @@
+
+-- #################################################################
 -- /qompassai/Diver/lsp/sq_ls.lua
--- Qompass AI SQL LSP Spec
+-- Qompass AI Diver SQL Language Server Config
 -- Copyright (C) 2025 Qompass AI, All rights reserved
--- --------------------------------------------------
---go install github.com/sqls-server/sqls@latest
+-- SPDX-License-Identifier: Apache-2.0
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at:
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+-- #################################################################
+---@source https://github.com/joe-re/sql-language-server
+---@source https://docs.sqlfluff.com/en/stable/
+
 ---@type vim.lsp.Config
 return {
-    cmd = { ---@type string[]
+    cmd = {
         'sql-language-server',
         'up',
         '--method',
         'stdio',
     },
-    filetypes = { ---@type string[]
-        'sql',
+
+    filetypes = {
         'mysql',
+        'pgsql',
+        'sql',
     },
-    root_markers = { ---@type string[]
+
+    root_markers = {
         '.sqllsrc.json',
+
+        {
+            '.sqlfluff',
+            'pyproject.toml',
+            'dbt_project.yml',
+        },
+
+        '.git',
     },
+
     settings = {
         sqlLanguageServer = {
-            connections = {
-                {
-                    driver = 'mysql',
-                    dataSourceName = 'root:root@tcp(127.0.0.1:13306)/world',
-                },
-                {
-                    driver = 'postgresql',
-                    dataSourceName = 'host=127.0.0.1 port=15432 user=postgres password=mysecretpassword1234 dbname=dvdrental sslmode=disable',
-                },
-            },
             lint = {
                 rules = {
-                    ['align-column-to-the-first'] = 'on',
-                    ['column-new-line'] = 'error',
-                    ['linebreak-after-clause-keyword'] = 'error',
-                    ['reserved-word-case'] = {
-                        'error',
-                        'upper',
-                    },
-                    ['space-surrounding-operators'] = 'error',
-                    ['where-clause-new-line'] = 'error',
-                    ['align-where-clause-to-the-first'] = 'error',
+                    ['align-column-to-the-first'] = 'off',
+
+                    ['align-where-clause-to-the-first'] = 'off',
+
+                    ['column-new-line'] = 'off',
+
+                    ['linebreak-after-clause-keyword'] = 'off',
+
+                    ['reserved-word-case'] = 'off',
+
+                    ['space-surrounding-operators'] = 'off',
+
+                    ['where-clause-new-line'] = 'off',
                 },
             },
         },
