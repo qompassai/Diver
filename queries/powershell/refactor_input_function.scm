afb1cc57db01c0e14dae082f45f396737acd792d
Matt Porter <map@qompass.ai>
2026-09-21 06:33:41 -0700

    Create refactor_input_function.scm

---
+++ queries/powershell/refactor_input_function.scm
@@ -0,0 +1,9 @@
+(program
+  (statement_list
+    (function_statement) @input_function))
+
+(program
+  (statement_list
+    (class_statement
+      (class_method_definition) @input_function))
+  (#set! method))
